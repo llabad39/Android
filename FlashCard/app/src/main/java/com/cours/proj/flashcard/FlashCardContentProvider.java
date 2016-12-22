bd088dba874d0d0630a097f45eccc19a296bc553
llabad39 <lucaslabadens@hotmail.fr>
2016-12-22 17:17:40 +0100

    query and update

--- FlashCard/app/src/main/java/com/cours/proj/flashcard/FlashCardContentProvider.java
+++ FlashCard/app/src/main/java/com/cours/proj/flashcard/FlashCardContentProvider.java
@@ -13,12 +13,14 @@ public class FlashCardContentProvider extends ContentProvider {
     private static final int DECK = 1;
     private static final int CARD = 2;
     private static final int ONE_CARD=3;
+    private static final int ONE_DECK=4;
     private static final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
     private BaseFlashCard bf ;
     static {
         matcher.addURI(authority, "deck_table", DECK);
         matcher.addURI(authority, "card_table", CARD);
         matcher.addURI(authority, "card/#", ONE_CARD);
+        matcher.addURI(authority, "deck/#",ONE_DECK);
     }
     public FlashCardContentProvider() {
     }
@@ -33,9 +35,22 @@ public class FlashCardContentProvider extends ContentProvider {
                 long idc = ContentUris.parseId(uri);
                 retour =db.delete("card_table","idCard = "+idc,null);
                 break;
+            case ONE_DECK:
+                long idd = ContentUris.parseId(uri);
+                SQLiteDatabase dr = bf.getReadableDatabase();
+                Cursor test = dr.query("card_table",null,"idDeck = " + idd,null,null,null,null);
+                if(test.getCount()==0) {
+                    retour = db.delete("deck_table", "idDeck = " + idd, null);
+
+                }
+                else
+                    retour = -2;
+                dr.close();
+                break;
             default:
                 throw new UnsupportedOperationException("Not yet implemented");
         }
+        db.close();
         return retour;
     }
 
@@ -48,20 +63,54 @@ public class FlashCardContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = bf.getWritableDatabase();
+        int ident =  matcher.match(uri);
+        Uri.Builder builder = new Uri.Builder();
+        long id;
+        switch (ident) {
+            case CARD:
+                id = db.insert("card_table",null,values);
+                builder.appendPath("card_table");
+                break;
+            case DECK:
+                id = db.insert("deck_table",null,values);
+                builder.appendPath("deck_table");
+                break;
+            default:
+                throw new UnsupportedOperationException("Not yet implemented");
+        }
+        builder.authority(authority);
+        builder = ContentUris.appendId(builder, id);
+        db.close();
+        return builder.build();
     }
 
     @Override
     public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
+        bf = BaseFlashCard.getInstance(getContext());
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
+        SQLiteDatabase dr = bf.getReadableDatabase();
+        int ident = matcher.match(uri);
+        Cursor c;
+        switch (ident) {
+            case CARD:
+                c = dr.query("card_table", projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case DECK:
+                c = dr.query("deck_table", projection, selection,
+                        selectionArgs, null, null, sortOrder);
+                break;
+            case ONE_CARD:
+                long idc = ContentUris.parseId(uri);
+                c = dr.query("card_table",new String[]{"question", "reponse"},"idCard = " + idc,selectionArgs,null,null,sortOrder);
+                break;
+        }
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
