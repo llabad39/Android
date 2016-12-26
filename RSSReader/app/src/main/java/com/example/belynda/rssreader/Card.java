cd4c901088a18bb062d42ee6d12896c73103cf69
llabad39 <lucaslabadens@hotmail.fr>
2016-12-26 15:50:34 +0100

    query hardest

---
+++ RSSReader/app/src/main/java/com/example/belynda/rssreader/Card.java
@@ -0,0 +1,27 @@
+package com.example.belynda.rssreader;
+
+/**
+ * Created by lucaslabadens on 26/12/2016.
+ */
+
+public class Card {
+    private String question;
+    private String answer;
+    private int id;
+    public Card(String q,String a,int i){
+        question=q;
+        answer=a;
+        id=i;
+    }
+    String getQuestion(){
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
