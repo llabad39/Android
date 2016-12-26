6742ce351a75a05e83f52f0bd2dfbbcb892efff6
belynda <belynda.hamaz@etu.univ-paris-diderot.fr>
2016-12-26 15:38:45 +0100

    mise à a jour

--- RSSReader/app/src/main/java/com/example/belynda/rssreader/RssfeedActivity.java
+++ RSSReader/app/src/main/java/com/example/belynda/rssreader/RssfeedActivity.java
@@ -4,32 +4,43 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class RssfeedActivity extends Activity implements
-        MyListFragment.OnItemSelectedListener  {
+public class RssfeedActivity extends Activity {
+    Button creer, jouer, supprimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rssfeed);
+        setContentView(R.layout.activity_menu);
+        creer = (Button) findViewById(R.id.creerJeu);
+        jouer = (Button) findViewById(R.id.jouer);
+        supprimer = (Button) findViewById(R.id.supprimerMenu);// qui se trouve dans le menu
+
     }
 
-    @Override
-    public void onRssItemSelected(String link) {
-        //Ajout du fragment de manière statique
-       DetailFragment fragment = (DetailFragment) getFragmentManager()
-              .findFragmentById(R.id.detailFragment);
-
-        if (fragment != null && fragment.isInLayout()) {
-            //**********************  cas ou onn est en paysage ********************/////
-            fragment.setText(link);
-        } else {
+    public void onClick(View v) {
+        if (v == jouer) {
             Intent intent = new Intent(getApplicationContext(),
-                    DetailActivity.class);
-            intent.putExtra(DetailActivity.EXTRA_URL, link);
+                    JeuExistantActivity.class);
+            // intent.putExtra(DetailActivity.EXTRA_URL, link);
             startActivity(intent);
-        }
+        }else
+            if(v == creer)
+            {
+                Intent intent2 = new Intent(getApplicationContext(),
+                        CreerActivity.class);
+                //intent.putExtra(DetailActivity.EXTRA_URL, link);
+                startActivity(intent2);
+            }else
+                if(v == supprimer)
+                {
+                    Intent intent3 = new Intent(getApplicationContext(),
+                            SupprimerActivity.class);
+                   // intent.putExtra(DetailActivity.EXTRA_URL, link);
+                    startActivity(intent3);
+                }
     }
-
-
 }
+
