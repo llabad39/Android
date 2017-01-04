f1b5fb8f8655af0a114ae32c064f93be73a48c19
llabad39 <lucaslabadens@hotmail.fr>
2017-01-04 18:15:18 +0100

    je

---
+++ RSSReader/app/src/main/java/com/example/belynda/rssreader/Deck.java
@@ -0,0 +1,22 @@
+package com.example.belynda.rssreader;
+
+/**
+ * Created by lucaslabadens on 03/01/2017.
+ */
+
+public class Deck {
+    private String name;
+    private int id;
+    public Deck(String n,int i){
+        name = n;
+        i= id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+}
